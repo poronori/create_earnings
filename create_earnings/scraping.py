020997c828d4d8b7f101aaedf492492d2e7d64a1
poronori <peacefireview@yahoo.co.jp>
2023-08-17 10:07:46 +0900

    Xpathで取得処理追加

--- create_earnings/scraping.py
+++ create_earnings/scraping.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from tkinter import messagebox
 from .scraping_data import ScrapingData
 
@@ -12,37 +13,73 @@ def open() :
     cur_url = driver.current_url
     print(cur_url)
     if "https://www.google.com/" in cur_url:
-        set_melcari_data()
+        set_melcari_data(driver)
     elif "https://www.yahoo.co.jp" in cur_url:
-        set_yahoo_data()
+        set_paypay_data(driver)
     else :
         messagebox.showwarning("URL不正", "メルカリかヤフーの取引画面を開いてください。")
     
     print('end open')
 
-
 # メルカリのデータ取得
-def set_melcari_data() :
-    date = '2023/8/1'
+def set_melcari_data(driver) :
+    date = '2023年8月14日 20:43'
     name = 'なんかの品名'
-    price = '1000'
+    price = '1,000'
     commission = '100'
-    customer = 'テスト 太郎'
-    address = '北海道 名古屋市 伏見'
+    customer = 'テスト 太郎 様'
+    postcode = '〒299-3189'
+    address1 = '北海道 名古屋市 伏見'
+    address2 = 'サンプル荘222'
+    address = address1 + ' ' + address2
     code = 'a99999999'
     
+    date = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div[1]/div/div/div[3]/div[5]/div[2]/span').text
+    price = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div[1]/div/div/div[3]/div[1]/div[2]/span/span/span[2]').text
+    commission = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div[1]/div/div/div[3]/div[2]/div[2]/span/span/span[2]').text
+    customer = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div[1]/div/div/div[4]/div/div[2]/span/div/p[4]').text
+    postcode = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div[1]/div/div/div[4]/div/div[2]/span/div/p[1]').text
+    address1 = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div[1]/div/div/div[4]/div/div[2]/span/div/p[2]').text
+    address2 = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div[1]/div/div/div[4]/div/div[2]/span/div/p[3]').text
+    code = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div[1]/div/div/div[3]/div[6]/div[2]/span/div/p').text
+    address = address1 + ' ' + address2
+    print(date)
+    print(price)
+    print(commission)
+    print(customer)
+    print(postcode)
+    print(address)
+    print(code)
+    
     scraping = ScrapingData()
-    scraping.setScrapingData(date, name, price, commission, customer, address, code)
+    scraping.setScrapingData(
+        date = date, 
+        name = name, 
+        price = price, 
+        commission = commission, 
+        customer = customer, 
+        postcode = postcode, 
+        address = address, 
+        code = code)
 
-# ヤフー？のデータ取得
-def set_yahoo_data() :
-    date = '2023/9/1'
-    name = 'ヤフーの品名'
-    price = '2000'
+# ペイペイフリマのデータ取得
+def set_paypay_data(driver) :
+    date = '2023年9月1日 12:05'
+    name = 'ペイペイの品名'
+    price = '2,000'
     commission = '100'
     customer = 'サンプル 花子'
+    postcode = '〒123-3456'
     address = '東京都 なんとか区 ほげほげ'
     code = 'b123456789'
     
     scraping = ScrapingData()
-    scraping.setScrapingData(date, name, price, commission, customer, address, code)
+    scraping.setScrapingData(
+        date = date, 
+        name = name, 
+        price = price, 
+        commission = commission, 
+        customer = customer, 
+        postcode = postcode, 
+        address = address, 
+        code = code)
--- create_earnings/scraping_data.py
+++ create_earnings/scraping_data.py
@@ -5,15 +5,17 @@ class ScrapingData :
     price = ''
     commission = ''
     customer = ''
+    postcode = ''
     address = ''
     code = ''
     
     @staticmethod
-    def setScrapingData(date, name, price, commission, customer, address, code):
+    def setScrapingData(date, name, price, commission, customer, postcode, address, code):
         ScrapingData.date = date
         ScrapingData.name = name
         ScrapingData.price = price
         ScrapingData.commission = commission
         ScrapingData.customer = customer
+        ScrapingData.postcode = postcode
         ScrapingData.address = address
         ScrapingData.code = code
